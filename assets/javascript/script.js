75363e653ef5404d2e71ce5a7a1350357c381ab8
pozhara <poliazhara04@gmail.com>
2022-11-05 11:25:41 +0000

    Add quiz.html

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+let question=document.getElementById("question");
+let questions=document.getElementById("questions");
+let option0=document.getElementById("option0");
+let option1=document.getElementById("option1");
+let option2=document.getElementById("option2");
+let next=document.getElementById("next");
+let stat=document.getElementById("stat");
+let score=document.getElementById("score");
+let scoreBtn=document.getElementById("score-btn");
+
