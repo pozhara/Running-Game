8c6ea5808478ae792a0a5b27db5be6c1c222418a
pozhara <poliazhara04@gmail.com>
2022-11-06 13:37:57 +0000

    Add end answers

--- assets/javascript/answers.js
+++ assets/javascript/answers.js
@@ -1,5 +1,22 @@
-let answers=[
-    {
-        
-    }
-]
+let answers= {
+    "j":{
+        "description": "You are Joel. You've been through tough times and it made you resourceful, practical and emotionally impenetrable, but you are still traumatized and vulnerable.",
+        "image":"joel.webp",
+    },
+    "d":{
+        "description":"You are Dina. You are warm, but you don't let people play with you. You're emphatic, you're able to hold space for people, even when you're dealing with your own pain. You are really devoted to people.",
+        "image":"dina.jpg",
+    },
+    "a":{
+        "description":"You are Abby. You're cheerful, hardworking and smart. You're a fun loving, yet a chaotic person. You can also be quite aggressive but you always make it up to people. You easily become angry at yourself if you make a mistake.",
+        "image":"abby.webp"
+    },
+    "e":{
+        "description":"You're strong, witty, sarcastic and a little rough around the edges. Because of your past, you grew up fast and now people describe you as 'mature beyond their years'. You're also impulsive, leading you to make rash decisions or saying wrong things.",
+        "image":"ellie.jpg",
+    },
+    "l":{
+        "description":"You are serious and pessimistic, but you have your moments of wonder and innocence. You are also pretty stubborn, going against what you're told to do or think. You know who you are and try to live life as the real you.",
+        "image":"lev.jpg",
+    },
+}
