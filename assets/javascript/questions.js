5279f7c90a54200f6ecb51f35e71a16d17b83265
pozhara <poliazhara04@gmail.com>
2022-11-06 13:17:26 +0000

    Write questions and answers

---
+++ assets/javascript/questions.js
@@ -0,0 +1,82 @@
+let quiz = [
+    {
+        "q": "If you could be anywhere, where would you be?",
+        "a": {
+            "j": "A small town in the Midwest",
+            "d": "A farm house with your lover",
+            "a": "An aquarium",
+            "e": "Abandoned ruins of Seattle",
+            "l": "In the forest by the shore",
+        }
+    },
+    {
+        "q": "Choose an animal",
+        "a": {
+            "j": "Magpie",
+            "d": "Hawk",
+            "a": "Wolf",
+            "e": "Giraffe",
+            "l": "Shark",
+        }
+    },
+    {
+        "q": "Choose your favourite drink",
+        "a": {
+            "j": "Black coffee",
+            "d": "Milkshake",
+            "a": "Tea",
+            "e": "Cola",
+            "l": "Hot chocolate",
+        }
+    },
+    {
+        "q": "What are you scared of most?",
+        "a": {
+            "j": "Never getting what you truly want",
+            "d": "Everyone leaving you",
+            "a": "Your loved ones dying",
+            "e": "Ending up alone",
+            "l": "Not being accepted in society",
+        }
+    },
+    {
+        "q": "What's your MBTI type?",
+        "a": {
+            "j": "ISTJ",
+            "d": "ESFP",
+            "a": "ESTP",
+            "e": "ENFP",
+            "l": "INFP",
+        }
+    },
+    {
+        "q": "What's your favourite color?",
+        "a": {
+            "j": "Brown",
+            "d": "Grey",
+            "a": "Blue",
+            "e": "Black",
+            "l": "Dark green",
+        }
+    },
+    {
+        "q": "Which line do you relate to?",
+        "a": {
+            "j": "Hurt and pain. There's much to gain. Peace and love. It's all the same.",
+            "d": "Hoping, dreaming on I stray to her smile, world away. Love unfulfilled, I rise and a world away, she cries",
+            "a": "Shock no thinking just decide. Rage from nowhere down inside. Sensing ruin, cannot tell, kill the threat or run like hell?",
+            "e": "Even when dreaming, I'm falling so fast, then walking last second, eyes wide. And feeling, after the terror has passed, a little emptier inside",
+            "l": "But my world fell apart, and all they could do, the whole universe, was to silently move on",
+        }
+    },
+    {
+        "q": "Which character not listed in the quiz do you like the most?",
+        "a": {
+            "j": "Tess",
+            "d": "Talia",
+            "a": "Owen",
+            "e": "JJ",
+            "l": "Yara",
+        }
+    },
+]
